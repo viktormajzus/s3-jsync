1213c4d8b90e5e08970031c94aba510d0da3fdba
viktormajzus <victor.mayzus@gmail.com>
2025-07-18 16:39:22 +0300

    Got rid of 2 unnecessary imports in main.

--- src/main/java/s3jsync/Main.java
+++ src/main/java/s3jsync/Main.java
@@ -1,8 +1,6 @@
 package s3jsync;
 
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
